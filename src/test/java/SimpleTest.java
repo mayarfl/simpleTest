c2d099c0dc8c7fc6ee646ceb670e749f10521961
dshmaya <dshmaya@users.noreply.github.com>
2018-03-11 13:14:46 +0200

    Update SimpleTest.java

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -29,7 +29,7 @@ public class SimpleTest {
   @Test
     public void alwaysPass4()
     {
-        Assert.assertEquals(1, 2;
+        Assert.assertEquals(1, 2);
     }
    @Test
    public void alwaysFail5()
@@ -37,13 +37,12 @@ public class SimpleTest {
        Assert.assertEquals(1, 2);
    }
 
-
-  @Test
+   @Test
     public void alwaysPass6()
     {
-        Assert.assertEquals(1, 1) ;
+        Assert.assertEquals(1, 2);
     }
-  /*	@Test  
+/* 	@Test
    public void alwaysPass7()
    {
        Assert.assertEquals(1, 1);
